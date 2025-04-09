475bc05daa44e5f4e393d4d9054901c7eec5c139
archana <archanavalsan2000@gmail.com>
2025-04-09 19:14:39 +0530

    added message

--- notes.md
+++ notes.md
@@ -11,3 +11,9 @@ Angular needs to know how the pieces of your application fit together and what o
 FormsModule in app.module.ts
 
 The \*ngFor is Angular's repeater directive. It repeats the host element for each element in a list.
+
+RxJS is a library for reactive programming using Observables, to make it easier to compose asynchronous or callback-based code. Reactive-Extensions
+
+Observable is one of the key classes in the RxJS library
+
+of(HEROES) returns an Observable<Hero[]> that emits a single value, the array of mock heroes.
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,10 +11,34 @@ import { HEROES } from '../mock-heroes';
   styleUrl: './heroes.component.css',
 })
 export class HeroesComponent {
-  heroes = HEROES;
+  constructor(
+    private heroService: HeroService,
+    private messageService: MessageService
+  ) {}
+  // heroes = HEROES;
+  heroes: Hero[] = [];
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
+  }
+  // getHeroes(): void {
+  //   this.heroes = this.heroService.getHeroes();
+  // }
+  ngOnInit(): void {
+    this.getHeroes();
+  }
   selectedHero?: Hero;
   onSelect(hero: Hero) {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
   // hero: Hero = { id: 1, name: 'Windstorm' };
 }
+
+// The previous version assigns an array of heroes to the component's heroes property.
+// The assignment occurs synchronously, as if the server could return heroes instantly
+// or the browser could freeze the UI while it waited for the server's response.
+// That won't work when the HeroService is actually making requests of a remote server.
+// The new version waits for the Observable to emit the array of heroes, which could
+// happen now or several minutes from now. The subscribe() method passes the emitted
+// array to the callback, which sets the component's heroes property.
+// This asynchronous approach works when the HeroService requests heroes from the server.
